884081f90b3f7b20deeac4ce9121d883aaa02731
hojat-gazestani <Hojat.Gazestani@gmail.com>
2022-02-18 02:51:18 +0330

    rabbitmq work queues

--- rabbitmq/1-Hello World/receive-1.py
+++ rabbitmq/1-Hello World/receive-1.py
@@ -4,12 +4,12 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hojat')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hojat', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
@@ -25,3 +25,4 @@ if __name__ == '__main__':
             os._exit(0)
 
 
+            
--- rabbitmq/1-Hello World/send.py
+++ rabbitmq/1-Hello World/send.py
@@ -6,6 +6,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='hello', body='New message from Hojat!')
+print(" [x] Sent 'New message'")
 connection.close()
